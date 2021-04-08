ea455f5867aa3b9a0ea90acda2060d996c895067
Atlas <doroshuk33@yandex.by>
2021-04-08 12:22:43 +0300

    update

--- app/index.php
+++ app/index.php
@@ -1401,7 +1401,7 @@
         (function(w,d,u){
             var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
             var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
-        })(window,document,'https://cdn-ru.bitrix24.ru/b16756392/crm/site_button/loader_1_s1sm3e.js');
+        })(window,document,'https://cdn-ru.bitrix24.ru/b16756178/crm/site_button/loader_2_bclsbm.js');
     </script>
 </body>
 </html>
